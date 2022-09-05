34086f3b6c35080f9d9fd99fa8d416eb4a0fca82
Braian <brahian0987@gmail.com>
2022-09-05 18:36:06 -0500

    Interfaces de los juegos terminadas

--- Parcial.py
+++ Parcial.py
@@ -1,132 +1,156 @@
-#import tkinter as tk
-#from tkinter import ttk, messagebox
 from tkinter import *
 from tkinter import messagebox
 
 listaJugadores = {}
 listaRespuestas = []
 
-def guardarRespuestaEntero(opcion):
-    if opcion.get() == 1 or opcion.get() == 2 or opcion.get() == 3 or opcion.get() == 4:
-        listaRespuestas.append(opcion)
-        messagebox.showinfo("Informacion", "Opcion guardada correctamente")
-    elif opcion.get() == 5 or opcion.get() == 6 or opcion.get() == 7 or opcion.get() == 10:
-        listaRespuestas.append(opcion)
-        messagebox.showinfo("Informacion", "Opcion guardada correctamente")
-
 def guardarRespuestaJuego1(opcion, ventana):
     if opcion.get() == 1 or opcion.get() == 2 or opcion.get() == 3 or opcion.get() == 4:
-        listaRespuestas.append(opcion)
+        listaRespuestas.append(opcion.get())
         ventana.destroy()
+        ingresarJuego2()
+    else:
+        messagebox.showerror("Error", "Por favor ingrese una de las opciones")
 
+def guardarRespuestaJuego2(opcion, ventana):
+    if opcion.get() == 2 or opcion.get() == 3 or opcion.get() == 4 or opcion.get() == 5:
+        listaRespuestas.append(opcion.get())
+        ventana.destroy()
+        ingresarJuego3()
+    else:
+        messagebox.showerror("Error", "Por favor ingrese una de las opciones")
 
-# def guardarRespuestaCadena(opcion):
-#     if opcion.get() == "Izquierda" or opcion.get() == "Derecha":
-#         listaRespuestas.append(opcion)
-#         messagebox.showinfo("Informacion", "Opcion guardada correctamente")
-#     elif opcion.get() == "Pueblo" or opcion.get() == "Elefante":
-#         listaRespuestas.append(opcion)
-#         messagebox.showinfo("Informacion", "Opcion guardada correctamente")
+def guardarRespuestaJuego3(opcion, ventana):
+    if opcion.get() == 5 or opcion.get() == 6 or opcion.get() == 7 or opcion.get() == 10:
+        listaRespuestas.append(opcion.get())
+        ventana.destroy()
+        ingresarJuego4()
+    else:
+        messagebox.showerror("Error", "Por favor ingrese una de las opciones")
 
-# def guardarRespuestasJugador():
-#     if len(listaRespuestas) == 5:
-#         listaJugadores[nombre] = listaRespuestas
+def guardarRespuestaJuego4(opcion, ventana):
+    if opcion.get() == "Izquierda" or opcion.get() == "Derecha":
+        listaRespuestas.append(opcion.get())
+        ventana.destroy()
+        ingresarJuego5()
+    else:
+        messagebox.showerror("Error", "Por favor ingrese una de las opciones")
 
-
-# def ingresarJuego():
-#     root.withdraw()
-#     #Ventana de los juegos
-#     juego = tk.Toplevel()
-#     juego.title("Juegos")
-#     juego.geometry("500x500")
-#     cuadroMadre = ttk.Notebook(juego)
-#     opcionEntero = tk.IntVar()
-#     opcionCadena = tk.StringVar()
-
-#     imagen1 = tk.PhotoImage(file="Parcial1IA/img/juego1.png")
-#     imagen2 = tk.PhotoImage(file="Parcial1IA/img/juego2.png")
-#     imagen3 = tk.PhotoImage(file="Parcial1IA/img/juego3.png")
-#     imagen4 = tk.PhotoImage(file="Parcial1IA/img/juego4.png")
-#     imagen5 = tk.PhotoImage(file="Parcial1IA/img/juego5.png")
-
-#     frame1 = ttk.Frame(cuadroMadre)
-#     frame2 = ttk.Frame(cuadroMadre)
-#     frame3 = ttk.Frame(cuadroMadre)
-#     frame4 = ttk.Frame(cuadroMadre)
-#     frame5 = ttk.Frame(cuadroMadre)
-
-#     frame1.pack(fill="both", expand=True)
-#     frame2.pack(fill="both", expand=True)
-#     frame3.pack(fill="both", expand=True)
-#     frame4.pack(fill="both", expand=True)
-#     frame5.pack(fill="both", expand=True)
-
-#     #Agregamos los frames al cuadro madre
-#     cuadroMadre.add(frame1, text="Juego 1")
-#     cuadroMadre.add(frame2, text="Juego 2")
-#     cuadroMadre.add(frame3, text="Juego 3")
-#     cuadroMadre.add(frame4, text="Juego 4")
-#     cuadroMadre.add(frame5, text="Juego 5")
-
-#     tk.Label(frame1, image=imagen1).pack(side=tk.TOP, pady=5, padx=5)
-#     tk.Label(frame1, text="¿Cuantos animales ves aqui?").pack()
-#     tk.Label(frame2, image=imagen2).pack(side=tk.TOP, pady=5, padx=5)
-#     tk.Label(frame2, text="¿Cuantas personas ves aqui?").pack()
-#     tk.Label(frame3, image=imagen3).pack(side=tk.TOP, pady=5, padx=5)
-#     tk.Label(frame3, text="¿Cuantos animales ves aqui?").pack()
-#     tk.Label(frame4, image=imagen4).pack(side=tk.TOP, pady=5, padx=5)
-#     tk.Label(frame4, text="¿Cual es mas grande?").pack()
-#     tk.Label(frame5, image=imagen5).pack(side=tk.TOP, pady=5, padx=5)
-#     tk.Label(frame5, text="¿Un pueblo o un elefante?").pack()
-
-#     tk.Radiobutton(frame1, text="1", variable=opcionEntero, value=1).pack()
-#     tk.Radiobutton(frame1, text="2", variable=opcionEntero, value=2).pack()
-#     tk.Radiobutton(frame1, text="3", variable=opcionEntero, value=3).pack()
-#     tk.Radiobutton(frame1, text="4", variable=opcionEntero, value=4).pack()
-#     tk.Radiobutton(frame2, text="2", variable=opcionEntero, value=2).pack()
-#     tk.Radiobutton(frame2, text="3", variable=opcionEntero, value=3).pack()
-#     tk.Radiobutton(frame2, text="4", variable=opcionEntero, value=4).pack()
-#     tk.Radiobutton(frame2, text="5", variable=opcionEntero, value=5).pack()
-#     tk.Radiobutton(frame3, text="5", variable=opcionEntero, value=5).pack()
-#     tk.Radiobutton(frame3, text="6", variable=opcionEntero, value=6).pack()
-#     tk.Radiobutton(frame3, text="7", variable=opcionEntero, value=7).pack()
-#     tk.Radiobutton(frame3, text="10", variable=opcionEntero, value=10).pack()
-#     tk.Radiobutton(frame4, text="Izquierda", variable=opcionCadena, value="Izquierda").pack()
-#     tk.Radiobutton(frame4, text="Derecha", variable=opcionCadena, value="Derecha").pack()
-#     tk.Radiobutton(frame5, text="Pueblo", variable=opcionCadena, value="Pueblo").pack()
-#     tk.Radiobutton(frame5, text="Elefante", variable=opcionCadena, value="Elefante").pack()
-
-#     ttk.Button(frame1, text="Guardar", command=lambda:guardarRespuestaEntero(opcionEntero)).pack(pady=5)
-#     ttk.Button(frame2, text="Guardar", command=lambda:guardarRespuestaEntero(opcionEntero)).pack(pady=5)
-#     ttk.Button(frame3, text="Guardar", command=lambda:guardarRespuestaEntero(opcionEntero)).pack(pady=5)
-#     ttk.Button(frame4, text="Guardar", command=lambda:guardarRespuestaCadena(opcionCadena)).pack(pady=5)
-#     ttk.Button(frame5, text="Guardar", command=lambda:guardarRespuestaCadena(opcionCadena)).pack(pady=5)
-
-#     cuadroMadre.pack(expand=True, fill=tk.BOTH, pady=5, padx=5)
-#     juego.mainloop()
+def guardarRespuestaJuego5(opcion, ventana):
+    if opcion.get() == "Pueblo" or opcion.get() == "Elefante":
+        listaRespuestas.append(opcion.get())
+        ventana.destroy()
+        root.deiconify()
+    else:
+        messagebox.showerror("Error", "Por favor ingrese una de las opciones")
 
 
 def ingresarJuego1():
-    #root.withdraw()
+    root.withdraw()
     juego1 = Toplevel()
     juego1.title("Juego 1")
     juego1.geometry("500x500")
+    opcion = IntVar()
 
     frame = Frame(juego1)
     frame.pack(fill="both", expand=True)
 
     imagenJuego1 = PhotoImage(file="Parcial1IA/img/juego1.png")
-    Label(frame, image=imagenJuego1).pack(side=TOP, pady=10)
+    Label(frame, image=imagenJuego1).pack(side=TOP, pady=10, padx=10)
     Label(frame, text="¿Cuantos animales ves aqui?").pack()
 
-    Radiobutton(frame, text="1", variable=opcionEntero, value=1).pack()
-    Radiobutton(frame, text="2", variable=opcionEntero, value=2).pack()
-    Radiobutton(frame, text="3", variable=opcionEntero, value=3).pack()
-    Radiobutton(frame, text="4", variable=opcionEntero, value=4).pack()
+    Radiobutton(frame, text="1", variable=opcion, value=1).pack()
+    Radiobutton(frame, text="2", variable=opcion, value=2).pack()
+    Radiobutton(frame, text="3", variable=opcion, value=3).pack()
+    Radiobutton(frame, text="4", variable=opcion, value=4).pack()
 
-    Button(frame, text="Guardar", command=lambda:guardarRespuestaJuego1(opcionEntero, juego1)).pack(pady=5)
+    Button(frame, text="Continuar", command=lambda:guardarRespuestaJuego1(opcion, juego1)).pack(pady=5)
 
     juego1.mainloop()
+
+def ingresarJuego2():
+    juego2 = Toplevel()
+    juego2.title("Juego 2")
+    juego2.geometry("500x500")
+    opcion = IntVar()
+
+    frame = Frame(juego2)
+    frame.pack(fill="both", expand=True)
+
+    imagenJuego2 = PhotoImage(file="Parcial1IA/img/juego2.png")
+    Label(frame, image=imagenJuego2).pack(side=TOP, pady=10)
+    Label(frame, text="¿Cuantas personas ves aqui?").pack()
+
+    Radiobutton(frame, text="2", variable=opcion, value=2).pack()
+    Radiobutton(frame, text="3", variable=opcion, value=3).pack()
+    Radiobutton(frame, text="4", variable=opcion, value=4).pack()
+    Radiobutton(frame, text="5", variable=opcion, value=5).pack()
+
+    Button(frame, text="Continuar", command=lambda:guardarRespuestaJuego2(opcion, juego2)).pack(pady=5)
+
+    juego2.mainloop()
+
+def ingresarJuego3():
+    juego3 = Toplevel()
+    juego3.title("Juego 3")
+    juego3.geometry("500x500")
+    opcion = IntVar()
+
+    frame = Frame(juego3)
+    frame.pack(fill="both", expand=True)
+
+    imagenJuego3 = PhotoImage(file="Parcial1IA/img/juego3.png")
+    Label(frame, image=imagenJuego3).pack(side=TOP, pady=10)
+    Label(frame, text="¿Cuantas animales ves aqui?").pack()
+
+    Radiobutton(frame, text="5", variable=opcion, value=5).pack()
+    Radiobutton(frame, text="6", variable=opcion, value=6).pack()
+    Radiobutton(frame, text="7", variable=opcion, value=7).pack()
+    Radiobutton(frame, text="10", variable=opcion, value=10).pack()
+
+    Button(frame, text="Continuar", command=lambda:guardarRespuestaJuego3(opcion, juego3)).pack(pady=5)
+
+    juego3.mainloop()
+    
+def ingresarJuego4():
+    juego4 = Toplevel()
+    juego4.title("Juego 4")
+    juego4.geometry("500x500")
+    opcion = StringVar()
+
+    frame = Frame(juego4)
+    frame.pack(fill="both", expand=True)
+
+    imagenJuego4 = PhotoImage(file="Parcial1IA/img/juego4.png")
+    Label(frame, image=imagenJuego4).pack(side=TOP, pady=10)
+    Label(frame, text="¿Cual es más grande?").pack()
+
+    Radiobutton(frame, text="Izquierda", variable=opcion, value="Izquierda").pack()
+    Radiobutton(frame, text="Derecha", variable=opcion, value="Derecha").pack()
+
+    Button(frame, text="Continuar", command=lambda:guardarRespuestaJuego4(opcion, juego4)).pack(pady=5)
+
+    juego4.mainloop()
+
+def ingresarJuego5():
+    juego5 = Toplevel()
+    juego5.title("Juego 5")
+    juego5.geometry("500x500")
+    opcion = StringVar()
+
+    frame = Frame(juego5)
+    frame.pack(fill="both", expand=True)
+
+    imagenJuego5 = PhotoImage(file="Parcial1IA/img/juego5.png")
+    Label(frame, image=imagenJuego5).pack(side=TOP, pady=10)
+    Label(frame, text="¿Un pueblo o un elefante?").pack()
+
+    Radiobutton(frame, text="Pueblo", variable=opcion, value="Pueblo").pack()
+    Radiobutton(frame, text="Elefante", variable=opcion, value="Elefante").pack()
+
+    Button(frame, text="Continuar", command=lambda:guardarRespuestaJuego5(opcion, juego5)).pack(pady=5)
+
+    juego5.mainloop()
 
 
 #Ventana de inicio
@@ -134,15 +158,13 @@ root = Tk()
 root.title("Juego de ilusiones opticas")
 frame = Frame(root)
 frame.pack()
-Label(frame, text="Ingresa tu nombre", font="curier 15").pack(anchor="center", pady=10)
+Label(frame, text="Ingresa tu nombre", font="curier 14").pack(anchor="center", pady=10)
 nombre = StringVar()
 entradaNombre = Entry(frame)
-entradaNombre.pack()
+entradaNombre.pack(padx=20)
 entradaNombre.config(bd=10, font=("curier", 20), textvariable=nombre)
 botonSalir = Button(frame, text="Jugar")
 botonSalir.pack(pady=10)
 botonSalir.config(bd=5, font=("curier", 10), command=ingresarJuego1)
 
-opcionEntero = IntVar()
-
-root.mainloop() 
+root.mainloop()
---
+++ README.md
@@ -0,0 +1,12 @@
+# Parcial I Inteligencia Artificial
+
+Este proyeto representa el codigo que desarrollamos para dar solucion al parcial del espacio academico Inteligencia artificial.
+
+## Los integrantes del grupo son:
+
+#### Braian Camilo Piedrahita Rodriguez - Fernando José Murcio Hincapié
+
+## Descripción del proyecto:
+
+El parcial consiste en un juego de ilusiones opticas en donde se le pondrán imagenes al usuario y este escoge
+lo que ve a partir de varias opciones de respuesta.
